08174032e064ee61fe49c8a8b1559081b01d2230
Danya Generalov <danya@danya02.ru>
2021-09-21 03:36:25 +0300

    Adding some code from https://github.com/pomf/pomf to keep similar style.

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM python:3.9
 COPY requirements.txt /
 RUN pip3 install -r /requirements.txt
 
+RUN wget -O /bootstrap.min.css https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css
+
+COPY static/ /static
 COPY templates/ /templates
 COPY main.py /
 
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
+
+@app.route('/style.css')
+def style():
+    return send_file('/bootstrap.min.css')
